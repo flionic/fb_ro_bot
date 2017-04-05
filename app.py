e9fb5f117014037564b9a847535fc8b2f99b11bd
Bionic-Leha <bionic.mods@gmail.com>
2017-04-06 02:25:19 +0300

    Del idea sys files

--- app.py
+++ app.py
@@ -138,6 +138,7 @@ def handle_incoming_messages():
 
 
 def web_process():
+
     if __name__ == '__main__':
         port = int(os.environ.get('PORT', 80))
         app.run(debug=True, host=os.environ.get('address', '0.0.0.0'), port=port)
