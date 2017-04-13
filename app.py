31747d1d1d7a4a3a71601b38edc16428a6e2af29
Bionic-Leha <bionic.mods@gmail.com>
2017-04-13 03:06:04 +0300

    Manage alerts (with share buttons)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 import requests
 import os
 import threading
-from messengerbot import MessengerClient, attachments, templates, elements
+from messengerbot import MessengerClient, attachments, templates, elements, webhooks
 import messages, quick_replies
 import MySQLdb
 
@@ -22,10 +22,10 @@ def get_posts(tid, pp):
 def db_query(uid, qid, sib=0):
     try:
         mysql = MySQLdb.connect(host=os.environ['DB_HOST'], user=os.environ['DB_USER'], password=os.environ['DB_PASS'],
-                                 db='fbmsgbot', autocommit=True)
+                                db='fbmsgbot', autocommit=True)
         sql_resp = [f'SELECT sub FROM bot_rol WHERE id=\'{uid}\'',
-                  f'INSERT INTO bot_rol (id, sub) VALUES (\'{int(uid)}\', \'{int(sib)}\')',
-                  f'UPDATE bot_rol SET sub={sib} WHERE id={uid})']
+                    f'INSERT INTO bot_rol (id, sub) VALUES (\'{int(uid)}\', \'{int(sib)}\')',
+                    f'UPDATE bot_rol SET sub={sib} WHERE id={uid})']
         with mysql.cursor() as cursor:
             sql = sql_resp[qid]
             cursor.execute(sql)
@@ -44,18 +44,142 @@ def send_fb_msg(user_id, msg):
     print(resp.content)
 
 
+def set_menu():
+    data = {
+        "setting_type": "call_to_actions",
+        "thread_state": "existing_thread",
+        "call_to_actions": [
+            {
+                "type": "postback",
+                "title": "Recent Lifestreams",
+                "payload": "MENU_LSTREAMS"
+            },
+            {
+                "type": "postback",
+                "title": "Hottest Stories",
+                "payload": "NENU_STORIES"
+            },
+            {
+                "type": "postback",
+                "title": "Send a message",
+                "payload": "MENU_SENDMSG"
+            },
+            {
+                "type": "postback",
+                "title": "Help",
+                "payload": "GET_HELP"
+            }
+        ]}
+    try:
+        print('Trying set menu: ')
+        resp = requests.post(
+            "https://graph.facebook.com/v2.6/me/thread_settings?access_token=" + os.environ['FACEBOOK_TOKEN'],
+            json=data)
+        print(resp.content)
+    except Exception as excp:
+        print(excp)
+
+
+def send_share(uid):
+    data = {
+        "recipient": {"id": uid},
+        "message": {"attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "Breaking News: Record Thunderstorms",
+                        "subtitle": "The local area is due for record thunderstorms over the weekend.",
+                        "image_url": "https://thechangreport.com/img/lightning.png",
+                        "buttons": [
+                            {
+                                "type": "element_share"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        }}
+    try:
+        print('Share')
+        resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + os.environ['FACEBOOK_TOKEN'],
+                             json=data)
+        print(resp.content)
+    except Exception as excp:
+        print(excp)
+
+
 # Init facebook client
 messenger = MessengerClient(access_token=os.environ['FACEBOOK_TOKEN'])
 
 
 def reply_lib(user_id, msg=None, pload=None, err=None):
+    if msg == 'set_menu':
+        set_menu()
     try:
         recipient = messages.Recipient(recipient_id=user_id)
         sub_id = db_query(user_id, 0)
+        ## BEGIN MENU ##
         if err:
             message = messages.Message(text=err)
             # elif sub_id is not None:
             # message = messages.Message(text=f'You are subscribed to: {sub_id}')
+        #############
+        elif msg == 'test':
+            pback_one = elements.PostbackButton(
+                title='Button One',
+                payload='PB_ONE'
+            )
+            pback_two = elements.PostbackButton(
+                title='Button Two',
+                payload='PB_TWO'
+            )
+            share_el = elements.Element(
+                title='Test GenericTemplate',
+                subtitle='Subtitle for this here.',
+                image_url='https://farbio.xyz/images/ava.jpg',
+                buttons=[pback_one, pback_two]
+            )
+            template = templates.GenericTemplate([share_el])
+            attachment = attachments.TemplateAttachment(template=template)
+            message = messages.Message(attachment=attachment)
+        #############
+        elif pload == 'MNG_ALERTS' or msg == 'mng':
+            pback_en_stor = elements.PostbackButton(
+                title='Enable Alerts',
+                payload='EN_SUB_STORIES'
+            )
+            pback_dis_stor = elements.PostbackButton(
+                title='Disable Alerts',
+                payload='DIS_SUB_STORIES'
+            )
+            el_stories = elements.Element(
+                title='Stories',
+                subtitle='Subtitle for this here.',
+                image_url='https://farbio.xyz/images/ava.jpg',
+                buttons=[pback_en_stor, pback_dis_stor]
+            )
+
+            pback_en_lstyle = elements.PostbackButton(
+                title='Enable Alerts',
+                payload='EN_SUB_LSTYLE'
+            )
+            pback_dis_lstyle = elements.PostbackButton(
+                title='Disable Alerts',
+                payload='DIS_SUB_LSTYLE'
+            )
+            el_lifestyle = elements.Element(
+                title='Life Programs',
+                subtitle='Subtitle for this here.',
+                image_url='https://getmdl.io/assets/demos/welcome_card.jpg',
+                buttons=[pback_en_lstyle, pback_dis_lstyle]
+            )
+            template = templates.GenericTemplate([el_stories, el_lifestyle])
+            attachment = attachments.TemplateAttachment(template=template)
+            message = messages.Message(attachment=attachment)
+        #############
         elif pload == 'WANT_SUB_STORIES':
             msg = f'You will start receiving the daily briefing\n' \
                   f'You can change your subscription at any time by typing "help"\n'
@@ -81,56 +205,36 @@ def reply_lib(user_id, msg=None, pload=None, err=None):
             )
             replies = quick_replies.QuickReplies(quick_replies=[qr_celebrity, qr_music, qr_rships, qr_lstyle])
             message = messages.Message(text='Would you like to see the hottest Stories now?', quick_replies=replies)
-    #############
+        #############
         elif pload == 'WANT_SUB_LIVEPROG':
             r_msg = f'Great! We`ll send you a message before our Live Program start\n' \
                     f'You can change your subscription at any time by typing \"help\"\n' \
                     f'Would you like to view our recent lifestreams?'
             messenger.send(messages.MessageRequest(recipient, messages.Message(text=r_msg)))
             pback_lives = elements.PostbackButton(
-                title='- Yes, sure',
+                title='Yes, sure',
                 payload='WANT_SUB_LIVE'
             )
             template = templates.ButtonTemplate(
-                text=f'------',
+                text=r_msg,
                 buttons=[pback_lives]
             )
             attachment = attachments.TemplateAttachment(template=template)
             message = messages.Message(attachment=attachment)
-    #############
-        elif pload == 'NOTHING_SUB':
-            pback_stories = elements.PostbackButton(
-                title='Hottest Stories',
-                payload='GET_STORIES'
-            )
-            pback_streams = elements.PostbackButton(
-                title='Recent Lifestreams',
-                payload='GET_STREAMS'
-            )
-            pback_help = elements.PostbackButton(
-                title='Help',
-                payload='GET_HELP'
-            )
-            template = templates.ButtonTemplate(
-                text='Main menu',
-                buttons=[pback_stories, pback_streams, pback_help]
-            )
-            attachment = attachments.TemplateAttachment(template=template)
-            message = messages.Message(attachment=attachment)
-    #############
+        #############
         else:
             r_msg = "Hi! Welcome to Radio One Lebanon Messanger. " \
                     "We'd love to share the hottest Celeb & Lifestyle Stories with you and notify you when our Live Programs start."
             pback_stories = elements.PostbackButton(
-                title="- Great, send me your best stories daily.",
+                title="stories",
                 payload='WANT_SUB_STORIES'
             )
             pback_liveprog = elements.PostbackButton(
-                title="- Love your Programs. Notify me when they start.",
+                title="programs",
                 payload='WANT_SUB_LIVEPROG'
             )
             pback_nosub = elements.PostbackButton(
-                title="- Not now, thank you",
+                title="not",
                 payload='NOTHING_SUB'
             )
             template = templates.ButtonTemplate(
@@ -139,11 +243,11 @@ def reply_lib(user_id, msg=None, pload=None, err=None):
             )
             attachment = attachments.TemplateAttachment(template=template)
             message = messages.Message(attachment=attachment)
-        # message = messages.Message(text=msg)
+        ## END OF MENU ##
         req = messages.MessageRequest(recipient, message)
         messenger.send(req)
     except Exception as excp:
-        print(excp)
+        print(f'Except sending message: {excp}')
 
 
 @app.route('/', methods=['GET'])
@@ -156,7 +260,7 @@ def verify():
         return request.args["hub.challenge"], 200
     return f'Python flask webhook listener on server: {os.environ["SERVER_NAME"]} - Active threads: {threading.active_count()}', 200
     # return redirect('http://farbio.xyz', 301)
-    #return render_template('index.html')
+    # return render_template('index.html')
 
 
 # noinspection PyBroadException
@@ -179,9 +283,15 @@ def handle_incoming_messages():
                 threading.Thread(target=reply_lib(sender, pload=pload)).start()
                 # reply_lib(sender, pload=pload)
             elif 'text' in msg:
-                message = msg['text'][::-1]
-                threading.Thread(target=reply_lib(sender, msg=message)).start()
-                # reply_lib(sender, msg=message)
+                message = msg['text']
+                # message = msg['text'][::-1]
+                if message == 'share1':
+                    send_share(sender)
+                elif message == 'share2':
+                    send_share2(sender)
+                else:
+                    threading.Thread(target=reply_lib(sender, msg=message)).start()
+                    # reply_lib(sender, msg=message)
     except Exception as excp:
         reply_lib(sender, err=f'Exception: {excp}')
     finally:
