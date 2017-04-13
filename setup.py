97ddeb2ae403a78caf7932b80a5d85e876ea36d1
Bionic-Leha <bionic.mods@gmail.com>
2017-04-13 06:29:00 +0300

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Facebook Messenger Bot',
+    version='0.3',
+    packages=[''],
+    url='https://farbio.xyz',
+    license='',
+    author='Bionic Leha',
+    author_email='i@farbio.xyz',
+    description=''
+)
